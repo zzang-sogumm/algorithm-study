55eb136230d20c1b13d05228b3cb17b4eb7c6ab2
Yeonju Jo <yjj0287@gmail.com>
2023-09-12 18:17:07 +0900

    refactor: BOJ 9465

--- yeonju/dynamic-programming/BOJ/9465/9465.py
+++ yeonju/dynamic-programming/BOJ/9465/9465.py
@@ -13,21 +13,18 @@ def main():
 
     for _ in range(t):
         n = read_int()
-        stickers = [read_int_list() for _ in range(2)]
-
-        dp = [[0] * (n + 3) for _ in range(2)]
-        dp[0][0] = stickers[0][0]
-        dp[1][0] = stickers[1][0]
+        dp = [read_int_list() for _ in range(2)]
 
         if n >= 2:
-            dp[0][1] = dp[1][0] + stickers[0][1]
-            dp[1][1] = dp[0][0] + stickers[1][1]
+            dp[0][1] += dp[1][0] 
+            dp[1][1] += dp[0][0] 
 
         for i in range(2, n):
-            dp[0][i] = max(dp[1][i - 1], dp[1][i - 2]) + stickers[0][i]
-            dp[1][i] = max(dp[0][i - 1], dp[0][i - 2]) + stickers[1][i]
+            dp[0][i] += max(dp[1][i - 1], dp[1][i - 2])
+            dp[1][i] += max(dp[0][i - 1], dp[0][i - 2])
 
-        print(max(dp[0][n - 1], dp[1][n - 1]))
+        result = max(dp[0][n - 1], dp[1][n - 1])
+        print(result)
 
 if __name__ == "__main__":
     main()

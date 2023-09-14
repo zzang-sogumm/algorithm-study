e57e2e556ae19417781e7ec2e9c08d26d7b746a9
mernyeok <jwh1124@naver.com>
2023-09-14 19:43:08 +0900

    refactor : 주석 추가

--- jwh/binary_search/BOJ2512/BOJ2512.java
+++ jwh/binary_search/BOJ2512/BOJ2512.java
@@ -23,25 +23,25 @@ class BOJ2512 {
 
         Arrays.sort(a); // 이분탐색을 위한 오름차순 정렬
 
-        e = a[n - 1];
+        e = a[n - 1]; // end값은 지방의 쵀대 요청금액
 
-        while (s <= e) {
+        while (s <= e) { // 최대 요청금액이 정해질때까지
             int sum = 0;
-            int mid = (s + e) / 2;
+            int mid = (s + e) / 2; // 중간 금액은 최소 + 최대 /2
 
             for (int i = 0; i < n; i++) {
-                if (a[i] >= mid) {
-                    sum += mid;
+                if (a[i] >= mid) { // 지방 요청금액이 중간보다 크다면?
+                    sum += mid; // 중간값을 더해줌
                 } else {
-                    sum += a[i];
+                    sum += a[i]; // 지방 요청금액이 중간보다 작다면 그냥 지방 요청금액을 더해줌
                 }
             }
 
-            if (sum > max_give) {
-                e = mid - 1;
-            } else if (sum <= max_give) {
+            if (sum > max_give) { // 위에 다 더했을 떄, 최대 예산보다 크다면?
+                e = mid - 1; // 중간금액이 작아져야 하니, 최대금액을 줄임
+            } else if (sum <= max_give) { // 그게 아니라면, 중간금액이 커져야하니 시작값을 늘림
                 s = mid + 1;
-                max = Math.max(max, mid);
+                max = Math.max(max, mid);// 중간금액을 키우면서 그 중 제일 큰 중간금액이 예산 중 최대값
             }
         }
 
